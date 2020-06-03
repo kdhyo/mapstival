5ca2c357dc091b2d3cec7fafbc943f0a82a6dd6a
Choi JiYoul <wldbf97@naver.com>
2020-06-03 13:02:13 +0900

    gmap fix

--- routes/gmap.js
+++ routes/gmap.js
@@ -3,20 +3,28 @@ const router = express.Router();
 const GAPI = require("../config/gapikey.json");
 const axios = require("axios");
 
-const GAPI_KEY = GAPI.GMAP_KEY;
+
+const GAPI_KEY = GAPI.GAPI_KEY;
 const lat = [];
 const equ = [];
 
-//축제 메인페이지 하기 gsdffefd
+//축제 메인페이지
 router.get("/main", function (req, res, next) {
-  //위도 경도 넣는곳
-  for (i = 0; i < 10; i++) {
-    lat.push(37.4 + i / 1000);
-    equ.push(126.93 + i / 1000);
-  }
-  res.render("mapstival/gmap", { lat: lat, equ: equ, GAPI_KEY: GAPI_KEY });
-  lat.length = 0;
-  equ.length = 0;
+
+    //위도 경도 넣는곳
+    for (i = 0; i < 10; i++) {
+        lat.push(37.395+i/1000);
+        equ.push(126.915+i/500);
+    }
+    res.render("mapstival/gmap", { lat: lat, equ:equ});
+    lat.length = 0;
+    equ.length = 0;
 });
+
+/////////////////////////////////
+
+
+//////////////////////////////////////
+
 
 module.exports = router;
