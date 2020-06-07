5ecaa3c2c490dd61ae26bc9512f77913b67f035f
Choi JiYoul <wldbf97@naver.com>
2020-06-07 23:37:47 +0900

    google review add

--- routes/gmap.js
+++ routes/gmap.js
@@ -9,7 +9,7 @@ const lat = [];
 const equ = [];
 
 //축제 메인페이지
-router.get("/main", function (req, res, next) {
+router.get("/", function (req, res, next) {
 
     //위도 경도 넣는곳
     for (i = 0; i < 10; i++) {
@@ -21,10 +21,36 @@ router.get("/main", function (req, res, next) {
     equ.length = 0;
 });
 
-/////////////////////////////////
-
-
-//////////////////////////////////////
-
+router.get("/review", function (req, res, next) {
+    axios
+      .get(`${"https://maps.googleapis.com/maps/api/place/search/json?location=37.402201,126.922347&radius=20&types=food&sensor=false&key=AIzaSyD7lGjC1U4IAhcB7eYjAph5r0lkEfKwKuY"}`)
+      .then((response) => {
+        const tourData = [];
+        // for (i = 0; i < 10; i++) {
+        //   tourData.push(response.data.response.body.items.item[i]);
+        // }
+          console.log(response.data.results[2].name);
+          console.log(response.data.results[2].reference);
+  
+        res.render("mapstival/greview", { data: tourData });
+      })
+      .catch((e) => {
+        res.send(e);
+      });
+    //   axios
+    //   .get(`${"https://maps.googleapis.com/maps/api/place/search/json?location=37.4020001,126.9220001&radius=20&types=food&sensor=false&key=AIzaSyD7lGjC1U4IAhcB7eYjAph5r0lkEfKwKuY"}`)
+    //   .then((response) => {
+    //     const tourData = [];
+    //     // for (i = 0; i < 10; i++) {
+    //     //   tourData.push(response.data.response.body.items.item[i]);
+    //     // }
+    //       console.log(response.data.results[1].name);
+  
+    //     res.render("mapstival/greview", { data: tourData });
+    //   })
+    //   .catch((e) => {
+    //     res.send(e);
+    //   });
+  });
 
 module.exports = router;
