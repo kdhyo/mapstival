71c8274ed42d05bfcf6765c8177b5a78a0126912
dongHyo <kdhyo98@gmail.com>
2020-08-18 19:37:37 +0900

    GoogleKey 빼버리기~

--- routes/mapstivals.js
+++ routes/mapstivals.js
@@ -163,53 +163,56 @@ const gmapUrl = [];
 //구글맵 이동
 router.get("/gmap", async function (req, res, next) {
   const gmapData = {
-    area : req.query.area,
-    date : req.query.date,
-  }
-  axios
-  await axios.get(
-    `${FESTIVAL_URL}searchFestival?serviceKey=${FESTIVAL_KEY}${FESTIVAL_ETC}&listYN=Y&areaCode=${gmapData.area}&pageNo=1&numOfRows=100&eventEndDate=${gmapData.date}`)
-  .then((response) => {
-    let tourData = [];
-    const list = response.data.response.body.items.item;
-    if (Array.isArray(list)) {
-      if (list != undefined) {
-        for (data in list) {
-          tourData.push(list[data]);
+    area: req.query.area,
+    date: req.query.date,
+  };
+  axios;
+  await axios
+    .get(
+      `${FESTIVAL_URL}searchFestival?serviceKey=${FESTIVAL_KEY}${FESTIVAL_ETC}&listYN=Y&areaCode=${gmapData.area}&pageNo=1&numOfRows=100&eventEndDate=${gmapData.date}`
+    )
+    .then((response) => {
+      let tourData = [];
+      const list = response.data.response.body.items.item;
+      if (Array.isArray(list)) {
+        if (list != undefined) {
+          for (data in list) {
+            tourData.push(list[data]);
+          }
+        } else {
+          tourData = null;
         }
-      } else {
+      } else if (response.data.response.body.items === "") {
         tourData = null;
+      } else {
+        tourData.push(list);
       }
-    } else if (response.data.response.body.items === "") {
-      tourData = null;
-    } else {
-      tourData.push(list);
-    }
-    //위도 경도 넣는곳
-    for (i = 0; i < tourData.length; i++) {
-      lat.push(parseFloat(tourData[i].mapy));
-      equ.push(parseFloat(tourData[i].mapx));
-      gmapTitle.push(tourData[i].title);
-      gmapUrl.push(tourData[i].mapx);
-      id.push(tourData[i].contentid);
-    }
-    res.render("mapstival/gmap", {
-      lat: lat,
-      equ: equ,
-      f_area: gmapData.area,
-      gmapTitle: gmapTitle,
-      tourData: tourData,
-      id: id,
+      //위도 경도 넣는곳
+      for (i = 0; i < tourData.length; i++) {
+        lat.push(parseFloat(tourData[i].mapy));
+        equ.push(parseFloat(tourData[i].mapx));
+        gmapTitle.push(tourData[i].title);
+        gmapUrl.push(tourData[i].mapx);
+        id.push(tourData[i].contentid);
+      }
+      res.render("mapstival/gmap", {
+        lat: lat,
+        equ: equ,
+        f_area: gmapData.area,
+        gmapTitle: gmapTitle,
+        tourData: tourData,
+        id: id,
+        API: GOOGLE_KEY,
+      });
+      lat.length = 0;
+      equ.length = 0;
+      gmapTitle.length = 0;
+      id.length = 0;
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send(e);
     });
-    lat.length = 0;
-    equ.length = 0;
-    gmapTitle.length = 0;
-    id.length = 0;
-  })
-  .catch((e) => {
-    console.log(e)
-    res.send(e);
-  });
 });
 
 module.exports = router;
